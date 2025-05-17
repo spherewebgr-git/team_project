73078b5941579a5524798b7d68679a30f535aad6
Spyrospapagiannakos <spyrospap1994@gmail.com>
2025-05-17 10:43:19 +0300

    seed admin user

---
+++ repo/database/seeders/UsersSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if(env('APP_ENV') != 'production')
+            {
+                $password = Hash::make('@dmin!@#');
+
+
+                    $users[] = [
+                        'name'=>'administrator',
+                        'email' =>'administrator@teamproject.com',
+                        'remember_token'=>\Ramsey\Uuid\Uuid::uuid4(),
+                        'email_verified_at'=>Carbon::now(),
+                        'created_at'=>Carbon::now(),
+                        'updated_at'=>Carbon::now(),
+                        'password' => $password
+                    ];
+
+
+                User::insert($users);
+            }
+    }
+}
