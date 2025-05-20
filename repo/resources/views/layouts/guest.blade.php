e706003dd565014f7f08c2ca0193e5d994fa5d26
Spyrospapagiannakos <spyrospap1994@gmail.com>
2025-05-20 22:43:45 +0300

    upload_assets

--- repo/resources/views/layouts/guest.blade.php
+++ repo/resources/views/layouts/guest.blade.php
@@ -1,20 +1,71 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+        <meta content="IE=edge" http-equiv="X-UA-Compatible">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Multipurpose, super flexible, powerful, clean modern responsive bootstrap 5 admin template"
+              name="description">
+        <meta content="admin template, ki-admin admin template, dashboard template, flat admin template, responsive admin template, web app"
+              name="keywords">
+        <meta content="la-themes" name="author">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" href="{{('../assets/images/logo/favicon.png')}}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{('../assets/images/logo/favicon.png')}}" type="image/x-icon">
+
+        <title>@yield('title') | ki-admin - Premium Admin Template</title>
+        <!-- Animation css -->
+        <link rel="stylesheet" href="{{ asset('assets/vendor/animation/animate.min.css') }}" >
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com" rel="preconnect">
+        <link crossorigin href="https://fonts.gstatic.com" rel="preconnect">
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+              rel="stylesheet">
 
-        <!-- Scripts -->
+        <!--Flag Icon css-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/flag-icons-master/flag-icon.css') }}">
+
+        <!-- Tabler icons-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/tabler-icons/tabler-icons.css') }}">
+
+        <!-- Prism css-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/prism/prism.min.css') }}">
+
+        <!-- Bootstrap css-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap/bootstrap.min.css') }}">
+
+        <!-- Simplebar css-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/simplebar/simplebar.css') }}">
+
+        @yield('css')
+        <link rel="stylesheet"  href="{{ asset('public/assets/scss/slyle.scss') }}">
 
     </head>
     <body class="font-sans text-gray-900 antialiased">
        @yield('content')
+
+       <!-- latest jquery-->
+       <script src="{{asset('assets/js/jquery-3.6.3.min.js')}}"></script>
+
+       <!-- Bootstrap js-->
+       <script src="{{asset('assets/vendor/bootstrap/bootstrap.bundle.min.js')}}"></script>
+
+       <!-- Simple bar js-->
+       <script src="{{asset('assets/vendor/simplebar/simplebar.js')}}"></script>
+
+       <!-- phosphor js -->
+       <script src="{{asset('assets/vendor/phosphor/phosphor.js')}}"></script>
+
+       <!-- Customizer js-->
+       <script src="{{asset('assets/js/customizer.js')}}"></script>
+
+       <!-- prism js-->
+       <script src="{{asset('assets/vendor/prism/prism.min.js')}}"></script>
+
+       <!-- App js-->
+       <script src="{{asset('assets/js/script.js')}}"></script>
+
+       @yield('script')
     </body>
 </html>
---
+++ repo/webpack.mix.cjs
@@ -0,0 +1,2 @@
+let mix = require('laravel-mix');
+mix.sass('resources/scss/style.scss','public/assets/css');
